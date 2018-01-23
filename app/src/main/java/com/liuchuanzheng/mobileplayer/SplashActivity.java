c133becea0eebf30f182f3962bdd5f24cd921b24
liuchuanzheng <1052374416@qq.com>
2018-01-23 22:36:41 +0800

    启动mainActivity

--- app/src/main/java/com/liuchuanzheng/mobileplayer/SplashActivity.java
+++ app/src/main/java/com/liuchuanzheng/mobileplayer/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.liuchuanzheng.mobileplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,18 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //延迟两秒启动 此方法在主线程
+                startMainActivity();
+            }
+        },2000);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        this.finish();
     }
 }
